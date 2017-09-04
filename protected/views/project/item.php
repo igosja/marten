7d2850fa7fb067794735b28c4e434be1e74ed6b2
Boyko Igor <igosja@ukr.net>
2017-09-04 15:04:57 +0300

    +

--- protected/views/project/item.php
+++ protected/views/project/item.php
@@ -3,6 +3,6 @@
  * @var $item Project
  */
 ?>
-<a href="javascript:" class="projects__i">
+<a href="<?= ImageIgosja::resize($item['image_id'], 600, 600); ?>" class="projects__i" data-lightbox="1">
     <img src="<?= ImageIgosja::resize($item['image_id'], 448, 448); ?>" alt="">
 </a>
